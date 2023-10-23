cb1dbdad9f68d13383f417b67b9ec83b48ff8dd8
lvlivingston <lvlivingston@gmail.com>
2023-10-23 10:45:25 -0500

    progress on lab part 3

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,25 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const ticketSchema = new Schema({
+    seat: {
+      type: String,
+      match: /[A-F][1-9]\d?/,
+      required: true
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true
+    }
+}, {
+    flight: {
+      type: Schema.Types.ObjectId,
+      ref: 'Flight',
+      required: true
+    }
+});
+
 const destinationsSchema = new Schema({
     destination: {
         type: String,
